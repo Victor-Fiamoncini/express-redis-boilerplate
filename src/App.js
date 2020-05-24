da547a776745ab5fa12da20881b6bc6180c4b3f0
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-05-24 20:50:00 -0300

    bull board

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import BullBoard from 'bull-board'
 
 import routes from './routes'
+import Queue from './app/lib/Queue'
 
 export default class App {
 	constructor() {
 		this.app = express()
 
 		this.middlewares()
+		this.queues()
 	}
 
 	get _app() {
@@ -19,7 +22,11 @@ export default class App {
 		this.app.use(express.json())
 		this.app.use(morgan('dev'))
 		this.app.use(cors())
-
 		this.app.use(routes)
+		this.app.use('/admin', BullBoard.UI)
+	}
+
+	queues() {
+		BullBoard.setQueues(Queue.queues.map((queue) => queue.bull))
 	}
 }
